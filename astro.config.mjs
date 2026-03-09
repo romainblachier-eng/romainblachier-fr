696424973a1db47b28470ab12d6973468383df65
romainblachier-eng <romain.blachier@gmail.com>
2026-03-09 14:33:40 +0100

    Fix build: remove TinaCMS, add site URL and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [],
+  site: 'https://romainblachier.fr',
+  integrations: [mdx(), sitemap()],
 });
